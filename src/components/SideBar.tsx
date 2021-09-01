18957aee399f3186ec438f3b685aedfcb1a24a8e
Isaias <isaiascxs10@gmail.com>
2021-09-01 16:21:36 -0300

    alteraçoes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  onGenres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  onHandleClickButton: (id: number) => void;
+  onSelectedGenreId: number
+}
+// Recebo o necessario utilizando props que estao devidamente tipadas igual no App
+export function SideBar({ onGenres, onHandleClickButton, onSelectedGenreId }: SideBarProps) {
+
+  return (
+
+
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {onGenres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onHandleClickButton(genre.id)}
+            selected={onSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+
+
+
+  )
+
 }
